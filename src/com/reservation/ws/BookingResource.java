496e0959f76d019eaa429003c5b98be0b6045137
HPBmla <bimlamadhavee@gmail.com>
2016-03-18 01:54:09 +0530

    More Web services

--- src/com/reservation/ws/BookingResource.java
+++ src/com/reservation/ws/BookingResource.java
@@ -1,9 +1,13 @@
 package com.reservation.ws;
+
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+
 import javax.ws.rs.Produces;
 
+import com.reservation.beans.ReservationBean;
 import com.reservation.beans.User;
 import com.reservation.service.HotelReservationServceImpl;
 
@@ -19,14 +23,35 @@ public class BookingResource {
 		return "Hello world";
 	}
 	@GET
-	@Produces("text/pliain")
-		public User login()
+	@Path("/login")
+	@Produces("text/plain")
+		public int login()
 	{
 	   HotelReservationServceImpl serviceLogin = new HotelReservationServceImpl();
 	   
-	   serviceLogin.login(user.getUsername(), user.getPassword());
-	   return user;
+	  int i = serviceLogin.login(user.getUsername(), user.getPassword());
+	   return i;
 	   
+	}
+	
+	@GET
+	@Path("/register")
+	@Produces("text/plain")
+    public int registration()
+	{
+		HotelReservationServceImpl serviceRegistr = new HotelReservationServceImpl();
+		int i= serviceRegistr.userRegistration(user);
+		return i;
+	}
+	
+	@GET
+	@Path("/viewBooking")
+	@Produces("text/plain")
+	public List<ReservationBean> getAllBookings()
+	{
+		HotelReservationServceImpl serviceBookings = new HotelReservationServceImpl();
+		List<ReservationBean> resrvation = serviceBookings.getAllBookings(user.getUserId());
+		return resrvation;
 	}
 	
 
